167d25e67dba19fc98e69da90ccd66489febed86
Mihir Bangia <89865393+MihirBangia@users.noreply.github.com>
2023-11-24 13:48:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.post("/login", async (req, res) => {
         );
         res.cookie("token", token, {
           httpOnly: true,
-          secure: false,
+          secure: true,
           sameSite:'none'
         });
         res.send("login success");
